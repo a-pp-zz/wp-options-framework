497320b3c16dae213d8e3808ffe69c91b9befa99
CoolSwitcher <CoolSwitcher@gmail.com>
2014-12-09 14:07:55 +0300

    Init

---
+++ js/maskedinput.handler.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($){
+   $(".wp-options-framework .masked_date").mask("99/99/9999");
+   $(".wp-options-framework .masked_datetime").mask("99/99/9999 99:99:99");
+   $(".wp-options-framework .masked_phone").mask("(999) 999-9999");
+});
